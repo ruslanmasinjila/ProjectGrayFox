ce1f9177d92128c6b6da9434a7a48109e402e5ce
ruslanmasinjila <ruslanmasinjila@gmail.com>
2023-02-09 12:54:18 +0300

    Focus on Trend + cutting through Kijun Sen and Tenkan Sen

--- ProjectGrayFox.py
+++ ProjectGrayFox.py
@@ -103,14 +103,20 @@ def getSignals(rates_frame,strTimeframe):
     # BUY SIGNAL
     #####################################################################################################
     if(tenkanSen_0 >= kijunSen_0):
+        if(senkouSpanA_0 > senkouSpanB_0):
             if(candleOpen_0 < kijunSen_0 and candleClose_0 > tenkanSen_0):
                 Signals.append("[BUY " + strTimeframe + "]")  
+
+        
     #####################################################################################################
     # SELL SIGNAL
     #####################################################################################################
     if(tenkanSen_0 <= kijunSen_0):
+        if(senkouSpanA_0 < senkouSpanB_0):
             if(candleOpen_0 > kijunSen_0 and candleClose_0 < tenkanSen_0):
-                Signals.append("[SELL " + strTimeframe + "]")              
+                Signals.append("[SELL " + strTimeframe + "]") 
+
+                  
 ##########################################################################################
 
 
