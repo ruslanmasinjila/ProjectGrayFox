2e9f3af2580a189afffe258ffdb70e83b507f412
ruslanmasinjila <ruslanmasinjila@gmail.com>
2023-02-18 00:21:52 +0300

    Nightly Push 20230218

--- ProjectGrayFox.py
+++ ProjectGrayFox.py
@@ -18,7 +18,8 @@ import os
 
 import winsound
 duration  = 50
-freq      = 1500
+freq1     = 2000
+freq2     = 1000
 
 # NUMBER OF COLUMNS TO BE DISPLAYED
 pd.set_option('display.max_columns', 500)
@@ -98,20 +99,27 @@ def getSignals(rates_frame,strTimeframe):
     previousLow               = rates_frame["low"].iloc[-2]
     previousIsGreen           = (previousClose > previousOpen)
     previousIsRed             = (previousClose < previousOpen)
-    currentSenkouSpanA        =  ichimokuValues[0]["ISA_9"].iloc[-1]      
-    currentSenkouSpanB        =  ichimokuValues[0]["ISB_26"].iloc[-1]
-    
+    previousSenkouSpanA       =  ichimokuValues[0]["ISA_9"].iloc[-2]      
+    previousSenkouSpanB       =  ichimokuValues[0]["ISB_26"].iloc[-2]
+ 
     
     # BUY SIGNAL
     if(currentLow  > previousLow and currentHigh > previousHigh):
         if(currentClose > previousHigh):
-            Signals.append("[BUY " + strTimeframe + "]")
+            if(((currentOpen  < currentSenkouSpanA  or currentOpen  < currentSenkouSpanB)  and (currentClose  > currentSenkouSpanA  and currentClose  > currentSenkouSpanB)) or
+               ((previousOpen < previousSenkouSpanA or previousOpen < previousSenkouSpanB) and (previousClose > previousSenkouSpanA and previousClose > previousSenkouSpanB))):
+                Signals.append("[BUY " + strTimeframe + " NOW]")
+            else:
+                Signals.append("[BUY " + strTimeframe + "]")
 
-            
     # SELL SIGNAL
     if(currentLow < previousLow and currentHigh < previousHigh):
         if(currentClose < previousLow):
-            Signals.append("[SELL " + strTimeframe + "]")
+            if(((currentOpen  > currentSenkouSpanA  or currentOpen  > currentSenkouSpanB)  and (currentClose  < currentSenkouSpanA  and currentClose  < currentSenkouSpanB)) or
+               ((previousOpen > previousSenkouSpanA or previousOpen > previousSenkouSpanB) and (previousClose < previousSenkouSpanA and previousClose < previousSenkouSpanB))):
+                Signals.append("[SELL " + strTimeframe + " NOW]")
+            else:
+                Signals.append("[SELL " + strTimeframe + "]")
 
                     
 ##########################################################################################
@@ -144,28 +152,31 @@ while(True):
         Signals =[]
         
         for t in range(len(mt5Timeframe)):
-            
             rates_frame = getRates(cp, mt5Timeframe[t], numCandles)
             getSignals(rates_frame,strTimeframe[t])
             
         sameSignals = []
-        if(len(Signals)>0):   
-            if(Signals[0]=="[BUY M1]"):
+        if(len(Signals)>0): 
+            if("BUY" in Signals[0]):
                 for i in Signals:
                     if("BUY" in i):
                         sameSignals.append(i)
                     else:
                         break
-            elif(Signals[0]=="[SELL M1]"):
+            elif("SELL" in Signals[0]):
                 for i in Signals:
                     if("SELL" in i):
                         sameSignals.append(i)
                     else:
                         break
-            if(len(sameSignals)>0):          
-                display+="***************************************************  "+ str(len(sameSignals))+"\n"+" ".join(sameSignals)+"\n"
-                winsound.Beep(freq, duration)
-                
+            if(len(sameSignals)>0):
+                if(sameSignals[0]=="[BUY M1 NOW]" or sameSignals[0]=="[SELL M1 NOW]"):
+                    display+="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX  "+ str(len(sameSignals))+"\n"+" ".join(sameSignals)+"\n"
+                    winsound.Beep(freq1, duration)
+                elif(sameSignals[0]=="[BUY M1]" or sameSignals[0]=="[SELL M1]"):
+                    display+="**************************************************  "+ str(len(sameSignals))+"\n"+" ".join(sameSignals)+"\n"
+                    winsound.Beep(freq2, duration)
+
         display+="==============================\n"
     print(display)
     time.sleep(5)
